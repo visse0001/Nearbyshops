9b4efc98665232bd49b41561dd4b9ade1d3b9862
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-03-02 11:56:41 +0100

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sandra/PycharmProjects/My_Django_stuff/Nearbyshops/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
